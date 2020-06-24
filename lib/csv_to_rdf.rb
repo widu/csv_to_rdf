0b076db41443700a7807d0631792ee6f25c8e655
WIDU <wdulek@gmail.com>
2020-06-24 21:12:53 +0200

    czerwiec 2020

--- lib/csv_to_rdf.rb
+++ lib/csv_to_rdf.rb
@@ -5,6 +5,15 @@ require 'linkeddata'
 
 class CsvToRdf
 
+##
+	# Build graph object from csv file
+	#  @param  [String]                 csv                
+	#  @param  [Hash{Symbol => Object}] csv_options    	- default = { :col_sep => ';', :headers => true}
+	#  @option csv_options [String]     :col_sep        - default = ';'
+	#  @option csv_options [Boolean]    :headers        - default = true
+    #  @return [RDF::Graph] - RDF graph contain information from csv file
+    #
+
 def csv_to_rdf(csv, csv_options = nil)
 	#{ :col_sep => ';', :headers => true}
 	csv_options ||= { :col_sep => ';', :headers => true}
